b97e81e906df21faa4c51fe1bdc9b870fa9e2ad7
St-evb <stevenverbruggen9@hotmail.com>
2025-08-05 11:07:40 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open('brandhulp-cache').then(function(cache) {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/css/style.css',
+                '/js/app.js'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
+});
